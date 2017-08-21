90e40a6db3149790949d7fa67d0e4abfff07f061
Charlie <charlie@skilbeck.com>
2017-08-21 18:10:27 +0100

    choco stuff

---
+++ Package/ReadMe.md
@@ -0,0 +1,2 @@
+## Summary
+It launches a web search for the selected text when you double tap ctrl-c
---
+++ Package/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$oReturn=[System.Windows.Forms.MessageBox]::Show("Message Text","Title",[System.Windows.Forms.MessageBoxButtons]::OKCancel)  
+switch ($oReturn){
+  "OK" {
+    write-host "You pressed OK"
+    # Enter some code
+  } 
+  "Cancel" {
+    write-host "You pressed Cancel"
+    # Enter some code
+  } 
+}
